39486c63dd919975bbeb4cd5033a752f9b308a87
Mardi Hach <mhach@uncc.edu>
2019-09-11 18:30:48 -0400

    Init commit

---
+++ AStar.java
@@ -0,0 +1,90 @@
+import java.util.*;
+
+/**
+ * This class generates a 15x15 double array full of traversable and blocked nodes.
+ * It uses the A* algorithm to find a path between the user specified starting
+ * and goal node.
+ * 
+ * @author Mardi
+ * @version 9/11/2019
+ */
+public class AStar {
+    private Node[][] board;
+    private PriorityQueue<Node> neighbors;
+    
+    /**
+     * Init constructor for Node board and neighbor queue
+     */
+    public AStar() {
+        board = new Node[15][15];
+        neighbors = new PriorityQueue<Node>();
+    }
+    
+    /**
+     * Generates a double array of Nodes
+     */
+    public void generateBoard() {
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 15; j++) {
+                board[i][j] = new Node(i, j, 0);
+            }
+        }
+    }
+    
+    /**
+     * Sets random nodes to be nontraversable
+     */
+    public void generateBlock() {
+        Random rand = new Random();
+        int i = rand.nextInt(15);
+        int j = rand.nextInt(15);
+        int count = 0;
+        while(count < 24) {
+            if(board[i][i].getType() == 0) {
+                board[i][j].setType(1);
+                i = rand.nextInt(15);
+                j = rand.nextInt(15);
+                count++;
+            }
+        }
+    }
+    
+    /**
+     * Prints a string representation of the board
+     */
+    public void printState() {
+        for(int i = 0; i < 15; i++) {
+            for(int j = 0; j < 15; j++) {
+                if(this.getNode(i, j).getType() == 0) {
+                    System.out.print("[ ]");
+                } else {
+                    System.out.print("[X]");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Returns node at index
+     * 
+     * @param int row of Node board
+     * @param int col of Node board
+     * @return Node at specified index
+     */
+    public Node getNode(int i, int j) {
+        return board[i][j];
+    }
+    
+    /**
+     * Main driver to execute the AStar class
+     */
+    public static void main(String[] args) {
+    	// TEST
+        AStar game = new AStar();
+        game.generateBoard();
+        game.generateBlock();
+        game.printState();
+    }
+    
+}
